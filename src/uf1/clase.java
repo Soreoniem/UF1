7ee6a362427fca9b232d7a98c5faa5bab7cafebf
Jµan Lµx <Jan Lx@Juan_Lux>
2014-09-26 16:55:39 +0200

    [26/09/2014] ╔═══════╗ ║  Gin Inicial  ║ ╚═══════╝ (probando caja ascii)

---
+++ src/uf1/clase.java
@@ -0,0 +1,10 @@
+package uf1;
+public class clase {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+        int res = n1 + n2;
+        System.out.println("El resultado es: " + res);
+    }
+    
+}
